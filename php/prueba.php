9a9c854b77156c1d74f87de202c65227c6d11fe1
jorcase <jorcascero@gmail.com>
2025-08-29 22:50:09 -0300

    Probando php

---
+++ php/prueba.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> Ejemplo PHP </title>
+    </head>
+    <body>
+        <?php
+            $copas = 6;
+            echo 'Cantidad de Copas Libertadores: <strong>' .
+            $copas . '</strong><br>';
+        ?>
+    </body>
+</html>
